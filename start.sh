28cfa333d82433d944801610dbf2561327cd0f32
animallibraryn4 <nikhil7858978052@gmail.com>
2025-12-23 13:53:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Check if libtorrent is installed
+python3.11 -c "import libtorrent" 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Installing python-libtorrent..."
+    pip3 install python-libtorrent
+fi
+
 python3.11 -m AAB
