74910cdb4c8e7c4d94096294cab7883e8b46456c
animallibraryn4 <nikhil7858978052@gmail.com>
2025-12-23 13:53:06 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
 # -- Important Build essential files -- # 
-
 sudo apt update
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
-sudo apt-get install -y software-properties-common
+sudo apt install -y \
+    build-essential \
+    zlib1g-dev \
+    libncurses5-dev \
+    libgdbm-dev \
+    libnss3-dev \
+    libssl-dev \
+    libreadline-dev \
+    libffi-dev \
+    libsqlite3-dev \
+    wlibbz2-dev \
+    wget \
+    software-properties-common \
+    libboost-all-dev \
+    libtorrent-rasterbar-dev
 
 # Install Python 3.11.3
-
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install -y python3.11
+sudo apt-get install -y python3.11 python3.11-dev
 
 # Installing ffmpeg 
-
 sudo apt install -y ffmpeg
 
-# Installing LibTorrent 
-
-sudo apt install -y python3-libtorrent
+# Update pip
+python3.11 -m pip install --upgrade pip
 
 # Installing Required Pypi Packages
-
-sudo python3.11 -m pip install -U -r requirements.txt
+python3.11 -m pip install -U -r requirements.txt
